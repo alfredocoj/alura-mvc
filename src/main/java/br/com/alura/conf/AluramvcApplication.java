bf53d75fcc6e3009a2d0a33e98ce7ae65787a3c4
root <admin@exemplo.com>
2017-03-06 13:45:38 -0300

    first commit

---
+++ src/main/java/br/com/alura/conf/AluramvcApplication.java
@@ -0,0 +1,12 @@
+package br.com.alura.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluramvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluramvcApplication.class, args);
+	}
+}
